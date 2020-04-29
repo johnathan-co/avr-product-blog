7a61504b23fd9849876dd42fa37a420e88addbe7
Johnathan Gonzalez <jgonzalez@avrub.com>
2020-04-29 15:01:45 -0500

    Edit Contact URL directs to AVR sites

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,7 +4,7 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
-    <Link to="/contact">Contact</Link>
+    <a href="https://www.avrub.com/contact">Contact</a>
     <ThemeChanger/>
   </nav>
   
