e397ca841e9693b914fe44602d4cebee4ecf5767
Johnathan Gonzalez <jgonzalez@avrub.com>
2020-04-29 11:23:19 -0500

    Editing contact page and config file

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -15,9 +15,9 @@ const ContactPage = ({
         <meta name="description" content={site.siteMetadata.description} />
       </Helmet>
       <div className="two-grids -contact">
-        <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
+        <div className="post-thumbnail" style={{backgroundImage: `url('/assets/AVRLOGOr.jpg')`, marginBottom: 0}}>
           <h1 className="post-title">Get in Touch</h1>
-          <p>Let me help you kick start your next project &rarr;</p>
+          <p>Have any questions for us? Let us know! &rarr;</p>
         </div>
         <div>
           <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
